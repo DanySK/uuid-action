b22e6c06a28ca1be3a926d3d9474970c63f59314
Danilo Pianini <danilo.pianini@gmail.com>
2025-01-25 23:08:04 +0100

    feat!: drop all inputs and use UUIDv7

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-const uuidv1 = require('uuid/v1');
-const uuidv5 = require('uuid/v5');
 const core = require('@actions/core');
+const { uuidv7 } = require('uuidv7');
 
 async function run() {
   try {
-    const name = core.getInput('name');
-    const namespace =
-      core.getInput('namespace') || 'f9962f80-1514-11ea-bfe7-1f9e6cf0a044';
-
-    let uuid = uuidv1();
-    if (name) {
-      uuid = uuidv5(name, namespace);
-    }
-
+    let uuid = uuidv7();
     core.setOutput('uuid', uuid);
   } catch (error) {
     core.setFailed(error.message);
--- release.config.mjs
+++ release.config.mjs
@@ -2,7 +2,14 @@ import config from 'semantic-release-preconfigured-conventional-commits' with { 
 
 config.plugins.push(
     "@semantic-release/github",
-    "@semantic-release/git",
+    [ "@semantic-release/git", {
+        assets: [
+            "dist/**/*.{js,css}",
+            "CHANGELOG.md",
+            "package.json",
+            "package-lock.json"
+        ]
+    } ]
 )
 
 export default config;
